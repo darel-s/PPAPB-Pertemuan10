bda136c8fb3ab01c7a896db6f40f65af74baee78
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-02 08:45:44 +0700

    buat recycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.recyclerview_pertemuan10"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.recyclerview_pertemuan10"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
