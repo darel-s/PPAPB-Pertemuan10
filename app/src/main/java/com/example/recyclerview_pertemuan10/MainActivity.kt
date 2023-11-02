d1f75b8d53801e7a934cf2b9ebd4874c842c296a
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-02 09:04:36 +0700

    student adapter and main activity

--- app/src/main/java/com/example/recyclerview_pertemuan10/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_pertemuan10/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.recyclerview_pertemuan10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview_pertemuan10.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var adapterStudent = StudentAdapter()
+        with(binding) {
+            rvStudent.apply {
+                adapter = adapterStudent
+                layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+        }
     }
 }
